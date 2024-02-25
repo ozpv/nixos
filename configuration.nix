85729a6c5bc755ddf2cccf37b1b83e460dd9663d
ozpv <39195175+ozpv@users.noreply.github.com>
2024-02-25 17:54:07 -0600

    fix pathing

--- configuration.nix
+++ configuration.nix
@@ -1,4 +1,4 @@
-# Edit this configuration file to define what should be installed on
+.# Edit this configuration file to define what should be installed on
 # your system.  Help is available in the configuration.nix(5) man page
 # and in the NixOS manual (accessible by running ‘nixos-help’).
 
@@ -91,7 +91,6 @@
     lf
     wget
     git
-    home-manager
   ];
 
   # browser
@@ -140,5 +139,4 @@
   # Before changing this value read the documentation for this option
   # (e.g. man configuration.nix or on https://nixos.org/nixos/options.html).
   system.stateVersion = "23.11"; # Did you read the comment?
-
 }
