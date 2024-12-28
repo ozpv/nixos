0c0630bc622b8c007a5a252a2ba32b1e22ab352b
ozpv <39195175+ozpv@users.noreply.github.com>
2024-12-28 02:07:18 -0600

    update zsh

--- home.nix
+++ home.nix
@@ -40,6 +40,10 @@
 
   programs.zsh = {
     enable = true;
+    enableCompletion = true;
+    autosuggestion.enable = true;
+    syntaxHighlighting.enable = true;
+
     shellAliases = {
       cp = "cp -v";
       mv = "mv -v";
@@ -47,26 +51,13 @@
       mkdir = "mkdir -v -p";
       rmdir = "rmdir -v";
       ls = "ls -hla --color=auto";
+      l = "ls -hla --color=auto";
+      ll = "ls -hla --color=auto";
       grep = "grep --color=auto";
-      vim-basic = "vim";
+      bvim = "vim";
       vim = "nvim";
     };
     
-    plugins = [
-      {
-        name = "zsh-autosuggestions";
-        src = fetchGit {
-          url = "https://github.com/zsh-users/zsh-autosuggestions.git";
-          rev = "c3d4e576c9c86eac62884bd47c01f6faed043fc5";
-        };
-      }
-      {
-        name = "zsh-syntax-highlighting";
-        src = fetchGit {
-          url = "https://github.com/zsh-users/zsh-syntax-highlighting.git";
-          rev = "e0165eaa730dd0fa321a6a6de74f092fe87630b0";
-        };
-      }
-    ];
+    history.size = 10000;
   };
 }
